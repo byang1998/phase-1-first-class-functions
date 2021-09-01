798adc7ed769d024125a55c3f3a535e5d0a8fbe8
Ben Yang <benyang2020@gmail.com>
2021-09-01 01:25:19 -0400

    commit tree adbd08b45bcc3e0221dba3a1bd837253611d22bc

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callbackfunction) {
+    callbackfunction();
+}
+
+function returnsANamedFunction()  {
+    return function names(){
+
+    }
+
+}
+  
+function returnsAnAnonymousFunction(){
+    return function() {
+    }
+}
